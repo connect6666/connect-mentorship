f0d853dc0f74b22936610aeaf234559e08bc1ae0
connect <136434302+connect6666@users.noreply.github.com>
2026-04-26 12:43:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,47 @@
-const CACHE_NAME = "connect-v1";
+const CACHE_NAME = "connect-v2";
 
-// Install
+const urlsToCache = [
+  "/connect-mentorship/",
+  "/connect-mentorship/index.html",
+  "/connect-mentorship/style.css"
+];
+
+// INSTALL
 self.addEventListener("install", (event) => {
+  console.log("Service Worker Installed");
   self.skipWaiting();
-});
 
-// Activate
-self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => caches.delete(key))
-      );
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Fetch (network first)
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated");
+
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  return self.clients.claim();
+});
+
+// FETCH (CACHE FIRST → FALLBACK NETWORK)
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
