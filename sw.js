668f17b86047ceba022ad1721eb3346c7bb898e0
connect <136434302+connect6666@users.noreply.github.com>
2026-04-30 15:04:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -40,8 +40,15 @@ self.addEventListener("activate", (event) => {
 // FETCH (CACHE FIRST → FALLBACK NETWORK)
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
